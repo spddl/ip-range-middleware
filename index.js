a201d33300d87079e80a861d2f5aa1d1b5767992
spddl <spddlhh@gmail.com>
2019-03-06 02:01:23 +0100

    add option: { debug: true }

--- index.js
+++ index.js
@@ -1,43 +1,58 @@
 'use strict'
 
-const checkRange = function (ip, Filter) {
-  for (let i = 0, len = Filter.length; i < len; i++) {
-    const Filterlen = Filter[i].length
-    if (Filterlen === 1) {
-      if (Filter[i][0] !== ip[i]) return false
-    } else if (Filterlen === 2) {
-      if (Filter[i][0] > ip[i] || Filter[i][1] < ip[i]) return false
+const checkRange = function (ip, filter) {
+  for (let i = 0, len = filter.length; i < len; i++) {
+    const filterlen = filter[i].length
+    if (filterlen === 1) {
+      if (filter[i][0] !== ip[i]) return false
+    } else if (filterlen === 2) {
+      if (filter[i][0] > ip[i] || filter[i][1] < ip[i]) return false
     } else {
-      throw new Error(Filter[i] + ' length is wrong')
+      throw new Error(filter[i] + ' length is wrong')
     }
   }
   return true
 }
 
-const checkIPRange = function (ip, Filter) {
-  if (!Array.isArray(Filter)) { return false }
+const checkIPRange = function (ip, filter, option = {}) {
+  if (!Array.isArray(filter)) { return false }
   if (!Array.isArray(ip)) {
     ip = ip.split('.').map(part => Number(part))
   }
-  if (Array.isArray(Filter[0][0])) {
-    for (let i = 0, len = Filter.length; i < len; i++) {
-      if (checkIPRange(ip, Filter[i])) return true
+
+  if (option.debug) { // this check is not default enabled for performance reasons
+    const isNotANumber = ip.find(part => isNaN(part))
+    if (isNotANumber) {
+      console.warn(`Ip Part ${isNotANumber}`)
+    }
+  }
+
+  if (Array.isArray(filter[0][0])) {
+    for (let i = 0, len = filter.length; i < len; i++) {
+      if (checkIPRange(ip, filter[i])) return true
     }
     return false
   } else {
-    return checkRange(ip, Filter)
+    return checkRange(ip, filter)
   }
 }
 
 module.exports = function (filter, option = {}) {
   return function (req, res, next) {
     req._ip = req.ip || req._remoteAddress || (req.connection && req.connection.remoteAddress) || undefined
-    if (option.allow && option.allow.indexOf(req._ip) !== -1) { return next() }
-    if (req._ip.substr(0, 7) === '::ffff:') req._ip = req._ip.substr(7)
-    if (checkIPRange(req._ip, filter)) {
+    if (option.allow && option.allow.indexOf(req._ip) !== -1) {
       next()
+      if (option.debug) console.log(`${option.allow[option.allow.indexOf(req._ip)]} Found, pass the gateway`)
     } else {
-      res.sendStatus(403)
+      if (req._ip.substr(0, 7) === '::ffff:') req._ip = req._ip.substr(7)
+      if (checkIPRange(req._ip, filter, option)) {
+        next()
+        if (option.debug) console.log(`${req._ip} whitelist ip`)
+      } else {
+        res.sendStatus(403)
+        if (option.debug) console.log(`${req._ip} 403`)
+      }
     }
+
   }
 }
