865ffa410820512bfe5e76b2e3d8ad33e354cb0b
spddl <spddlhh@gmail.com>
2019-03-06 01:08:43 +0100

    add more tests

--- test/test.js
+++ test/test.js
@@ -3,72 +3,138 @@
 const IPRange = require('../index.js')
 const assert = require('assert')
 
-describe('#indexOf()', function () {
-  const localhost = IPRange([[127], [0], [0], [1]])
-  it(`IPRange([[127], [0], [0], [1]]) ip: '127.0.0.1'`, function (done) {
+describe(`IPRange([
+    [127], [0], [0], [1]
+  ])`, function () {
+
+  const localhost = IPRange([
+    [127], [0], [0], [1]
+  ])
+
+  it(`127.0.0.1 => next()`, function (done) {
     localhost({ ip: '127.0.0.1' }, { sendStatus: function () {} }, function () {
       done()
     })
   })
-  it(`IPRange([[127], [0], [0], [1]]) ip: '127.0.0.2'`, function (done) {
+  it(`127.0.0.2 => 403`, function (done) {
     localhost({ ip: '127.0.0.2' }, { sendStatus: function (statusCode) {
       assert.equal(403, statusCode)
       done()
     } }, function () {})
   })
+})
 
-  const localhost2 = IPRange([[127], [0], [0]])
-  it(`IPRange([[127], [0], [0]]) ip: '127.0.0.1'`, function (done) {
+describe(`IPRange([
+    [127], [0], [0]
+  ])`, function () {
+
+  const localhost2 = IPRange([
+    [127], [0], [0]
+  ])
+
+  it(`127.0.0.1 => next()`, function (done) {
     localhost2({ ip: '127.0.0.1' }, { sendStatus: function () {} }, function () {
       done()
     })
   })
-  it(`IPRange([[127], [0], [0]]) ip: '127.0.0.255'`, function (done) {
+  it(`127.0.0.255 => next()`, function (done) {
     localhost2({ ip: '127.0.0.255' }, { sendStatus: function () {} }, function () {
       done()
     })
   })
+})
 
-  const ip1 = IPRange([[[127], [0], [0]], [[255], [0], [0]]])
-  it(`IPRange([[[127], [0], [0]], [[255], [0], [0]]]) ip: '127.0.0.1'`, function (done) {
+describe(`IPRange([
+    [[127], [0], [0]],
+    [[255], [0], [0]]
+  ])`, function () {
+
+  const ip1 = IPRange([
+    [[127], [0], [0]],
+    [[255], [0], [0]]
+  ])
+
+  it(`127.0.0.1 => next()`, function (done) {
     ip1({ ip: '127.0.0.1' }, { sendStatus: function () {} }, function () {
       done()
     })
   })
-  it(`IPRange([[[127], [0], [0]], [[255], [0], [0]]]) ip: '250.0.0.255'`, function (done) {
+  it(`I250.0.0.255 => 403`, function (done) {
     ip1({ ip: '250.0.0.255' }, { sendStatus: function (statusCode) {
       assert.equal(403, statusCode)
       done()
     } }, function () {})
   })
-  it(`IPRange([[[127], [0], [0]], [[255], [0], [0]]]) ip: '255.0.0.255'`, function (done) {
+  it(`255.0.0.255 => next()`, function (done) {
     ip1({ ip: '255.0.0.255' }, { sendStatus: function () {} }, function () {
       done()
     })
   })
+})
 
-  const ip2 = IPRange([[127], [0], [0, 50]], { allow: ['::1'] })
-  it(`IPRange([[127], [0], [0, 50]], { allow: ['::1'] }) ip: '127.0.0.1'`, function (done) {
+describe(`IPRange([
+    [127], [0], [0, 50]
+  ],
+  { allow: ['::1'] })`, function () {
+
+  const ip2 = IPRange([
+    [127], [0], [0, 50]
+  ], { allow: ['::1'] })
+
+  it(`127.0.0.1 => next()`, function (done) {
     ip2({ ip: '127.0.0.1' }, { sendStatus: function () { } }, function () {
       done()
     })
   })
-  it(`IPRange([[127], [0], [0, 50]], { allow: ['::1'] }) ip: '127.0.15.1'`, function (done) {
+  it(`127.0.15.1 => next()`, function (done) {
     ip2({ ip: '127.0.15.1' }, { sendStatus: function () { } }, function () {
       done()
     })
   })
-  it(`IPRange([[127], [0], [0, 50]], { allow: ['::1'] }) ip: '127.0.151.1'`, function (done) {
+  it(`127.0.151.1 => 403`, function (done) {
     ip2({ ip: '127.0.151.1' }, { sendStatus: function (statusCode) {
       assert.equal(403, statusCode)
       done()
     } }, function () {})
   })
 
-  it(`IPRange([[127], [0], [0, 50]], { allow: ['::1'] }) ip: '::1'`, function (done) {
+  it(`::1`, function (done) {
     ip2({ ip: '::1' }, { sendStatus: function () { } }, function () {
       done()
     })
   })
+})
 
+describe(`IPRange([
+    [[192], [168], [178], [2]],
+    [[127], [0], [0], [1]]
+  ])`, function () {
+
+  const ip3 = IPRange([
+    [[192], [168], [178], [2]],
+    [[127], [0], [0], [1]]
+  ])
+
+  it(`127.0.0.1 => next()`, function (done) {
+    ip3({ ip: '127.0.0.1' }, { sendStatus: function () { } }, function () {
+      done()
+    })
+  })
+  it(`192.168.178.2 => next()`, function (done) {
+    ip3({ ip: '192.168.178.2' }, { sendStatus: function () { } }, function () {
+      done()
+    })
+  })
+  it(`127.0.151.1 => 403`, function (done) {
+    ip3({ ip: '127.0.151.1' }, { sendStatus: function (statusCode) {
+      assert.equal(403, statusCode)
+      done()
+    } }, function () {})
+  })
+  it(`192.168.178.1 => 403`, function (done) {
+    ip3({ ip: '192.168.178.1' }, { sendStatus: function (statusCode) {
+      assert.equal(403, statusCode)
+      done()
+    } }, function () {})
+  })
 })
